cfe7f5bb560101566ef37ffe00c9e03aade99dd6
Md kaunain raza <2403051050332@paruluniversity.ac.in>
2025-12-27 08:39:32 +0530

    Create RockPaperScissorsGame.java

---
+++ RockPaperScissorsGame.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+       char p1, p2;
+       p1 = s.next().charAt(0);
+       p2 = s.next().charAt(0);
+       if(p1 == 'R' && p2 == 'P') System.out.println("p2 Wins");
+       else if(p1 == 'R' && p2 == 'P') System.out.println("p1 Wins");
+       else if(p1 == 'P' && p2 == 'S') System.out.println("p2 Wins");
+       else if(p1 == 'S' && p2 == 'P') System.out.println("p1 Wins");
+       else if(p1 == 'S' && p2 == 'R') System.out.println("p2 Wins");
+       else if(p1 == 'R' && p2 == 'S') System.out.println("p1 Wins");
+       else if(p1 == p2) System.out.println("Ties");
+       else System.out.println("Invalid Input");
+    }
+Ro
